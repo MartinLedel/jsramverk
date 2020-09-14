142c6d645d3f723d30a2a6dabd7359d8608ed5fb
MartinLedel <martinledel@gmail.com>
2020-09-14 08:43:42 +0200

    Fixed infinite callback loop on kmom view

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,19 +1,20 @@
 import React, { useEffect, useState } from 'react';
 
 const Me = () => {
-  const [message, setMessage] = useState('');
-  useEffect(() => {
-  fetch(`http://localhost:1337/`)
-      .then(res => res.json())
-      .then(res => setMessage(res.data.text));
-  });
+    const [message, setMessage] = useState('');
+    useEffect(() => {
+        fetch(`http://localhost:1337/`)
+            .then(res => res.json())
+            .then(res => setMessage(res.data.text));
+    }, []);
+    console.log(message);
 
-  return (
+    return (
     <main>
       <h1>Me</h1>
       <p>{message}</p>
     </main>
-  );
+    );
 };
 
 export default Me;
--- src/components/Report.js
+++ src/components/Report.js
@@ -3,21 +3,20 @@ import React, { useEffect, useState } from 'react';
 const Report = ({ match }) => {
     const kmom = match.params.kmom ;
     const [text, setText] = useState([]);
-
     useEffect(() => {
-    fetch(`http://localhost:1337/reports/week/${kmom}`)
-        .then(res => res.json())
-        .then(res => setText(res.data));
-    });
-
-  return (
+        fetch(`http://localhost:1337/reports/week/${kmom}`)
+            .then(res => res.json())
+            .then(res => setText(res.data));
+    }, [kmom]);
+    console.log(text);
+    return (
     <main>
       <h2>{ "kmom0" + kmom }</h2>
       <div className="display-linebreak">
         { text.texts }
      </div>
     </main>
-  );
+    );
 };
 
 export default Report;
